495ccab0ac50628f1f050a1c72084c84fb46da30
juboraj <juborajahmed0213@gmail.com>
2025-01-21 14:21:17 +0600

    Updated file

--- weather_project/forecast/urls.py
+++ weather_project/forecast/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('', views.index, name='index'),
 ]
--- weather_project/forecast/views.py
+++ weather_project/forecast/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from .utils import get_weather_data
 
 def index(request):
@@ -7,3 +6,35 @@ def index(request):
         city = request.GET['city']
         weather_data = get_weather_data(city)
     return render(request, 'forecast/index.html', {'weather_data': weather_data})
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'forecast/signup.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'forecast/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
